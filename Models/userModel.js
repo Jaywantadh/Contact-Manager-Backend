cd3b7ce79e82a19bfe9fc00134c3145c6ebb2659
Jaywant Adhau <119965656+Jaywantadh@users.noreply.github.com>
2024-02-29 20:10:03 +0530

    Add files via upload

---
+++ Models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the user name"],
+    },
+    email: {
+        type: String,
+        required: [true, "please add the user email address"],
+        unique: [true, "Email address is already taken"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please add the user password"],
+    }
+}, {
+        timstamps: true,
+});
+
+module.exports = mongoose.model("User", userSchema);
